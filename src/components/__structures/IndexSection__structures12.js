937d9a17a501be4ca896a18ba83d3992f8702691
salemaziel <51006863+salemaziel@users.noreply.github.com>
2024-09-03 12:45:04 +0000

    Published from Shuffle Editor

---
+++ src/components/__structures/IndexSection__structures12.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function IndexSection__structures12() {
+    return (
+        <React.Fragment>
+            <>
+                <div className="bg-[#F2A809] dark:bg-[#111827] relative">  <div className="container mx-auto">    <div className="max-w-max lg:max-w-3xl mx-auto">      <a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2048.2222241727227!2d-117.10182485206931!3d32.74575493125262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d9541492b869ab%3A0x88e5ef22dc35c99c!2sCotija's%20Mex%20Grill%20and%20Seafood!5e0!3m2!1sen!2sus!4v1691640682123!5m2!1sen!2sus" rel="nofollow noreferer" className="overflow-hidden my-0 pb-6 w-3/4 max-w-screen-lg justify-center mx-auto rounded-3xl" style={{boxShadow: 'rgba(0, 0, 0, 0.05) 0px 0px 0px 1px', filter: 'drop-shadow(rgba(0, 0, 0, 0.6) 2px 2px 4px)'}}>        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2048.2222241727227!2d-117.10182485206931!3d32.74575493125262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d9541492b869ab%3A0x88e5ef22dc35c99c!2sCotija's%20Mex%20Grill%20and%20Seafood!5e0!3m2!1sen!2sus!4v1691640682123!5m2!1sen!2sus" loading="lazy" width="100%" height="100%" className="block p-0 mx-auto mt-6 rounded-3xl mb-6 w-full align-middle border-0 border-gray-200 border-solid h-full" style={{height: '60vh'}} />        <div className="flex items-center p-5 border-0 bg-white rounded-3xl border-gray-200 border-solid">          <i className="p-5 mr-5 italic rounded-xl border-0 border-gray-200 border-solid bg-zinc-100">            <svg width={28} height={28} viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg" className="block italic align-middle border-0 border-gray-200 border-solid">              <path d="M14 1.75C11.4482 1.75301 9.00181 2.76804 7.19742 4.57242C5.39304 6.37681 4.37801 8.82322 4.375 11.375C4.37194 13.4603 5.05311 15.4891 6.314 17.15C6.314 17.15 6.5765 17.4956 6.61938 17.5455L14 26.25L21.3841 17.5411C21.4226 17.4948 21.686 17.15 21.686 17.15L21.6869 17.1474C22.9471 15.4872 23.628 13.4594 23.625 11.375C23.622 8.82322 22.607 6.37681 20.8026 4.57242C18.9982 2.76804 16.5518 1.75301 14 1.75ZM14 14.875C13.3078 14.875 12.6311 14.6697 12.0555 14.2851C11.4799 13.9006 11.0313 13.3539 10.7664 12.7144C10.5015 12.0749 10.4322 11.3711 10.5673 10.6922C10.7023 10.0133 11.0356 9.38961 11.5251 8.90013C12.0146 8.41064 12.6383 8.0773 13.3172 7.94225C13.9961 7.8072 14.6999 7.87651 15.3394 8.14142C15.9789 8.40633 16.5256 8.85493 16.9101 9.4305C17.2947 10.0061 17.5 10.6828 17.5 11.375C17.4988 12.3029 17.1297 13.1925 16.4736 13.8486C15.8175 14.5047 14.9279 14.8738 14 14.875Z" fill="#FD2F25" className="italic border-0 border-gray-200 border-solid" />          </svg>        </i>          <span className="font-medium border-0 border-gray-200 border-solid">3695 Fairmount Ave, San Diego, CA 92105</span>      </div>    </a>  </div></div></div>
+
+
+            </>
+        </React.Fragment>
+    );
+}
+
---
+++ src/components/testimonials-dark-color/IndexSectionTestimonialsDarkColor5.js
@@ -0,0 +1,185 @@
+import React from 'react';
+
+export default function IndexSectionTestimonialsDarkColor5() {
+    return (
+        <React.Fragment>
+            <>
+                <section>
+  <div className="skew skew-top mr-for-radius">
+    <svg className="h-8 md:h-12 lg:h-20 w-full text-gray-900" viewBox="0 0 10 10" preserveAspectRatio="none">
+      <polygon fill="currentColor" points="0 0 10 10 0 10" />
+    </svg>
+  </div>
+  <div className="skew skew-top ml-for-radius">
+    <svg className="h-8 md:h-12 lg:h-20 w-full text-gray-900" viewBox="0 0 10 10" preserveAspectRatio="none">
+      <polygon fill="currentColor" points="0 10 10 0 10 10" />
+    </svg>
+  </div>
+  <div className="py-20 bg-gray-900 radius-for-skewed overflow-hidden">
+    <div className="max-w-6xl mx-auto px-4 pb-6 lg:pb-16">
+      <div className="flex flex-wrap justify-center lg:justify-between items-center text-center lg:text-left">
+        <div className="w-full lg:w-4/5 mb-4 lg:mb-0">
+          <span className="text-purple-600 font-bold">Dolor sit amet consectutar</span>
+          <h2 className="text-4xl lg:text-5xl font-bold text-white">John Doe</h2>
+        </div>
+        <div className="w-full lg:w-1/5">
+          <button className="mr-4 bg-white p-5 rounded-full shadow-md text-purple-600 hover:text-purple-700 transition duration-200">
+            <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+            </svg>
+          </button>
+          <button className="bg-white p-5 rounded-full shadow-md text-purple-600 hover:text-purple-700 transition duration-200">
+            <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div className="relative flex">
+      <div className="hidden lg:block absolute top-0 left-0 lg:w-1/3 px-3 -ml-80 opacity-25">
+        <div className="p-8 bg-white rounded shadow text-center">
+          <div className="bg-no-repeat bg-left-top" style={{backgroundImage: 'url("atis-assets/elements/quote-grey.svg")'}}>
+            <p className="mb-8 text-gray-500 leading-loose">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et placerat metus. Morbi aliquet felis sit amet erat finibus, ac condimentum ligula ornare.</p>
+            <img className="mb-2 mx-auto w-12 h-12 rounded-full object-cover" src="https://images.unsplash.com/photo-1516321318423-f06f85e504b3?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMzIzMzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MjM3NzEwOTN8&ixlib=rb-4.0.3&q=85&w=80&h=23" alt />
+            <h4 className="mb-1 text-2xl font-bold font-heading">Sarah Johnson</h4>
+            <p className="text-gray-500">Backend Developer</p>
+          </div>
+        </div>
+      </div>
+      <div className="overflow-hidden max-w-6xl px-2 mx-auto">
+        <div className="whitespace-nowrap transition-transform duration-500 ease-in-out">
+          <div className="inline-block whitespace-normal">
+            <div className="flex flex-wrap">
+              <div className="mb-4 w-full lg:w-1/3 px-3">
+                <div className="p-8 bg-white rounded shadow text-center">
+                  <div className="bg-no-repeat bg-left-top" style={{backgroundImage: 'url("atis-assets/elements/quote-grey.svg")'}}>
+                    <p className="mb-8 text-gray-500 leading-loose">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et placerat metus. Morbi aliquet felis sit amet erat finibus, ac condimentum ligula ornare.</p>
+                    <img className="mb-2 mx-auto w-12 h-12 rounded-full object-cover" src="https://images.unsplash.com/photo-1556740720-776b84291f8e?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMzIzMzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MjM3NzEwOTN8&ixlib=rb-4.0.3&q=85&w=80&h=23" alt />
+                    <h4 className="mb-1 text-2xl font-bold font-heading">Michael Smith</h4>
+                    <p className="text-gray-500">Backend Developer</p>
+                  </div>
+                </div>
+              </div>
+              <div className="mb-4 w-full lg:w-1/3 px-3">
+                <div className="p-8 bg-white rounded shadow text-center">
+                  <div className="bg-no-repeat bg-left-top" style={{backgroundImage: 'url("atis-assets/elements/quote-grey.svg")'}}>
+                    <p className="mb-8 text-gray-500 leading-loose">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et placerat metus. Morbi aliquet felis sit amet erat finibus, ac condimentum ligula ornare.</p>
+                    <img className="mb-2 mx-auto w-12 h-12 rounded-full object-cover" src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80" alt />
+                    <h4 className="mb-1 text-2xl font-bold font-heading">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4>
+                    <p className="text-gray-500">Frontend Developer</p>
+                  </div>
+                </div>
+              </div>
+              <div className="mb-4 w-full lg:w-1/3 px-3">
+                <div className="p-8 bg-white rounded shadow text-center">
+                  <div className="bg-no-repeat bg-left-top" style={{backgroundImage: 'url("atis-assets/elements/quote-grey.svg")'}}>
+                    <p className="mb-8 text-gray-500 leading-loose">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et placerat metus. Morbi aliquet felis sit amet erat finibus, ac condimentum ligula ornare.</p>
+                    <img className="mb-2 mx-auto w-12 h-12 rounded-full object-cover" src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt />
+                    <h4 className="mb-1 text-2xl font-bold font-heading">Frontend Developer</h4>
+                    <p className="text-gray-500">Product Developer</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="inline-block whitespace-normal">
+            <div className="flex flex-wrap">
+              <div className="mb-4 w-full lg:w-1/3 px-3">
+                <div className="p-8 bg-white rounded shadow text-center">
+                  <div className="bg-no-repeat bg-left-top" style={{backgroundImage: 'url("atis-assets/elements/quote-grey.svg")'}}>
+                    <p className="mb-8 text-gray-500 leading-loose">Hasellus sollicitudin massa ut quam aliquam, feugiat fringilla est aliquam. Phasellus tincidunt mi a pellentesque fermentum. Quisque vitae erat iaculis, porttitor elit sed, viverra nisl. Maecenas facilisis viverra sollicitudin.</p>
+                    <img className="mb-2 mx-auto w-12 h-12 rounded-full object-cover" src="https://images.unsplash.com/photo-1484588168347-9d835bb09939?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt />
+                    <h4 className="mb-1 text-2xl font-bold font-heading">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</h4>
+                    <p className="text-gray-500">Data Analyst</p>
+                  </div>
+                </div>
+              </div>
+              <div className="mb-4 w-full lg:w-1/3 px-3">
+                <div className="p-8 bg-white rounded shadow text-center">
+                  <div className="bg-no-repeat bg-left-top" style={{backgroundImage: 'url("atis-assets/elements/quote-grey.svg")'}}>
+                    <p className="mb-8 text-gray-500 leading-loose">Hasellus sollicitudin massa ut quam aliquam, feugiat fringilla est aliquam. Phasellus tincidunt mi a pellentesque fermentum. Quisque vitae erat iaculis, porttitor elit sed, viverra nisl. Maecenas facilisis viverra sollicitudin.</p>
+                    <img className="mb-2 mx-auto w-12 h-12 rounded-full object-cover" src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80" alt />
+                    <h4 className="mb-1 text-2xl font-bold font-heading">UI/UX Designer</h4>
+                    <p className="text-gray-500">UX Designer</p>
+                  </div>
+                </div>
+              </div>
+              <div className="mb-4 w-full lg:w-1/3 px-3">
+                <div className="p-8 bg-white rounded shadow text-center">
+                  <div className="bg-no-repeat bg-left-top" style={{backgroundImage: 'url("atis-assets/elements/quote-grey.svg")'}}>
+                    <p className="mb-8 text-gray-500 leading-loose">Hasellus sollicitudin massa ut quam aliquam, feugiat fringilla est aliquam. Phasellus tincidunt mi a pellentesque fermentum. Quisque vitae erat iaculis, porttitor elit sed, viverra nisl. Maecenas facilisis viverra sollicitudin.</p>
+                    <img className="mb-2 mx-auto w-12 h-12 rounded-full object-cover" src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt />
+                    <h4 className="mb-1 text-2xl font-bold font-heading">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
+                    <p className="text-gray-500">DevOps Engineer</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="inline-block whitespace-normal">
+            <div className="flex flex-wrap">
+              <div className="mb-4 w-full lg:w-1/3 px-3">
+                <div className="p-8 bg-white rounded shadow text-center">
+                  <div className="bg-no-repeat bg-left-top" style={{backgroundImage: 'url("atis-assets/elements/quote-grey.svg")'}}>
+                    <p className="mb-8 text-gray-500 leading-loose">Ut auctor libero sed dolor venenatis, non hendrerit enim volutpat. Proin ac enim eget dui luctus bibendum. Sed fringilla dui ac nulla consectetur, vitae eleifend tortor hendrerit.</p>
+                    <img className="mb-2 mx-auto w-12 h-12 rounded-full object-cover" src="https://images.unsplash.com/photo-1484588168347-9d835bb09939?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt />
+                    <h4 className="mb-1 text-2xl font-bold font-heading">Data Scientist</h4>
+                    <p className="text-gray-500">Software Engineer</p>
+                  </div>
+                </div>
+              </div>
+              <div className="mb-4 w-full lg:w-1/3 px-3">
+                <div className="p-8 bg-white rounded shadow text-center">
+                  <div className="bg-no-repeat bg-left-top" style={{backgroundImage: 'url("atis-assets/elements/quote-grey.svg")'}}>
+                    <p className="mb-8 text-gray-500 leading-loose">Ut auctor libero sed dolor venenatis, non hendrerit enim volutpat. Proin ac enim eget dui luctus bibendum. Sed fringilla dui ac nulla consectetur, vitae eleifend tortor hendrerit.</p>
+                    <img className="mb-2 mx-auto w-12 h-12 rounded-full object-cover" src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80" alt />
+                    <h4 className="mb-1 text-2xl font-bold font-heading">4.9</h4>
+                    <p className="text-gray-500">Cybersecurity Specialist</p>
+                  </div>
+                </div>
+              </div>
+              <div className="mb-4 w-full lg:w-1/3 px-3">
+                <div className="p-8 bg-white rounded shadow text-center">
+                  <div className="bg-no-repeat bg-left-top" style={{backgroundImage: 'url("atis-assets/elements/quote-grey.svg")'}}>
+                    <p className="mb-8 text-gray-500 leading-loose">Ut auctor libero sed dolor venenatis, non hendrerit enim volutpat. Proin ac enim eget dui luctus bibendum. Sed fringilla dui ac nulla consectetur, vitae eleifend tortor hendrerit.</p>
+                    <img className="mb-2 mx-auto w-12 h-12 rounded-full object-cover" src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt />
+                    <h4 className="mb-1 text-2xl font-bold font-heading">4.8</h4>
+                    <p className="text-gray-500">Frontend Developer</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="hidden lg:block absolute top-0 right-0 lg:w-1/3 px-3 -mr-80 opacity-25">
+        <div className="p-8 bg-white rounded shadow text-center">
+          <div className="bg-no-repeat bg-left-top" style={{backgroundImage: 'url("atis-assets/elements/quote-grey.svg")'}}>
+            <p className="mb-8 text-gray-500 leading-loose">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et placerat metus. Morbi aliquet felis sit amet erat finibus, ac condimentum ligula ornare.</p>
+            <img className="mb-2 mx-auto w-12 h-12 rounded-full object-cover" src="https://images.unsplash.com/photo-1484588168347-9d835bb09939?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt />
+            <h4 className="mb-1 text-2xl font-bold font-heading">4.9</h4>
+            <p className="text-gray-500">Backend Developer</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="skew skew-bottom mr-for-radius">
+    <svg className="h-8 md:h-12 lg:h-20 w-full text-gray-900" viewBox="0 0 10 10" preserveAspectRatio="none">
+      <polygon fill="currentColor" points="0 0 10 0 0 10" />
+    </svg>
+  </div>
+  <div className="skew skew-bottom ml-for-radius">
+    <svg className="h-8 md:h-12 lg:h-20 w-full text-gray-900" viewBox="0 0 10 10" preserveAspectRatio="none">
+      <polygon fill="currentColor" points="0 0 10 0 10 10" />
+    </svg>
+  </div>
+</section>
+
+
+            </>
+        </React.Fragment>
+    );
+}
+
