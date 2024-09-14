644b764a10416abc52e11bf104b3f9cf3c62f741
salemaziel <mymainemail0501@gmail.com>
2024-09-13 22:06:26 -0700

    Add .gitignore file and update tailwind.config.js - Added .gitignore file to ignore node_modules, .lh, and .vscode directories. - Updated tailwind.config.js to include additional breakpoints for responsive design.

--- src/tailwind/tailwind.config.js
+++ src/tailwind/tailwind.config.js
@@ -7,7 +7,13 @@ module.exports = {
                         "sm": "640px",
                         "md": "768px",
                         "lg": "1024px",
-                        "xl": "1140px"
+                        "xl": "1140px",
+                        "2xl": "1280px",
+                        "3xl": "1440px",
+                        "4xl": "1600px",
+                        "5xl": "1920px",
+                        "6xl": "2560px",
+                        "7xl": "3840px"
                     },
                     "colors": {
                         "current": "currentColor",
